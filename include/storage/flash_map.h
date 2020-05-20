97d2073c3507f72672358c0dd260a425b68b9ba4
Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
2020-05-20 22:37:09 +0200

    [nrf noup] re apply partition manager patches
    Re apply diff in flash_map_default.c and create
    partition manager variants of FLASH_AREA macros in
    flash_map.h.
    
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

--- include/storage/flash_map.h
+++ include/storage/flash_map.h
@@ -209,6 +209,25 @@ int flash_area_has_driver(const struct flash_area *fa);
  */
 struct device *flash_area_get_device(const struct flash_area *fa);
 
+#if USE_PARTITION_MANAGER
+#include <pm_config.h>
+#include <sys/util.h>
+
+#define PM_ID(label) PM_##label##_ID
+
+#define FLASH_AREA_ID(label) PM_ID(label)
+
+#define FLASH_AREA_OFFSET(label) \
+	UTIL_CAT(PM_, UTIL_CAT(UTIL_CAT(PM_, UTIL_CAT(PM_ID(label), _LABEL)), _OFFSET))
+
+#define FLASH_AREA_SIZE(label) \
+	UTIL_CAT(PM_, UTIL_CAT(UTIL_CAT(PM_, UTIL_CAT(PM_ID(label), _LABEL)), _SIZE))
+
+#define FLASH_AREA_LABEL_EXISTS(label) \
+	IS_ENABLED(FLASH_AREA_SIZE(label))
+
+#else
+
 #define FLASH_AREA_LABEL_EXISTS(label) \
 	DT_HAS_FIXED_PARTITION_LABEL(label)
 
@@ -220,6 +239,8 @@ struct device *flash_area_get_device(const struct flash_area *fa);
 
 #define FLASH_AREA_SIZE(label) \
 	DT_REG_SIZE(DT_NODE_BY_FIXED_PARTITION_LABEL(label))
+
+#endif /* USE_PARTITION_MANAGER */
 
 #ifdef __cplusplus
 }
