nrfconnect/sdk-zephyr

[nrf noup] dfu/boot/mcuboot: fix confirmation in case of USE_PARTITION_MANAGER
Active partition ID need to be extracted basing on PARTITION_MANAGER
products.

ref.:NCSDK-26693

Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

subsys/dfu/boot/mcuboot.c
@@ -34,8 +34,34 @@
 #define BOOT_HEADER_MAGIC_V1 0x96f3b83d
 #define BOOT_HEADER_SIZE_V1 32
 
+#if USE_PARTITION_MANAGER
+#include <pm_config.h>
+
+#if CONFIG_BUILD_WITH_TFM
+	#define PM_ADDRESS_OFFSET (PM_MCUBOOT_PAD_SIZE + PM_TFM_SIZE)
+#else
+	#define PM_ADDRESS_OFFSET (PM_MCUBOOT_PAD_SIZE)
+#endif
+
+#ifdef CONFIG_MCUBOOT
+	/* lib is part of MCUboot -> operate on the primart application slot */
+	#define ACTIVE_SLOT_ID		PM_MCUBOOT_PRIMARY_ID
+#else
+	/* lib is part of the App -> operate on active slot */
+#if (PM_ADDRESS - PM_ADDRESS_OFFSET) == PM_MCUBOOT_PRIMARY_ADDRESS
+	#define ACTIVE_SLOT_ID		PM_MCUBOOT_PRIMARY_ID
+#elif (PM_ADDRESS - PM_ADDRESS_OFFSET) == PM_MCUBOOT_SECONDARY_ADDRESS
+	#define ACTIVE_SLOT_ID		PM_MCUBOOT_SECONDARY_ID
+#else
+	#error Missing partition definitions.
+#endif
+#endif /* CONFIG_MCUBOOT */
+
+#define ACTIVE_SLOT_FLASH_AREA_ID ACTIVE_SLOT_ID
+#else
 /* Get active partition. zephyr,code-partition chosen node must be defined */
 #define ACTIVE_SLOT_FLASH_AREA_ID DT_FIXED_PARTITION_ID(DT_CHOSEN(zephyr_code_partition))
+#endif /* USE_PARTITION_MANAGER */
 
 /*
  * Raw (on-flash) representation of the v1 image header.

tejlmand/fw-nrfconnect-zephyr-1

Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>
(cherry picked from commit deb2aa894b1dc1eea2bb36a39106f2714d1b75a5)

subsys/dfu/boot/mcuboot.c
@@ -49,6 +49,48 @@ enum IMAGE_INDEXES {
 	IMAGE_INDEX_2
 };
 
+#if USE_PARTITION_MANAGER
+#include <pm_config.h>
+
+#if CONFIG_MCUBOOT_APPLICATION_IMAGE_NUMBER != -1
+/* Sysbuild */
+#ifdef CONFIG_MCUBOOT
+/* lib is part of MCUboot -> operate on the primary application slot */
+#define ACTIVE_SLOT_FLASH_AREA_ID	PM_MCUBOOT_PRIMARY_ID
+#else
+/* TODO: Add firmware loader support */
+/* lib is part of the app -> operate on active slot */
+#if defined(CONFIG_NCS_IS_VARIANT_IMAGE)
+#define ACTIVE_SLOT_FLASH_AREA_ID	PM_MCUBOOT_SECONDARY_ID
+#else
+#define ACTIVE_SLOT_FLASH_AREA_ID	PM_MCUBOOT_PRIMARY_ID
+#endif
+#endif /* CONFIG_MCUBOOT */
+#else
+/* Legacy child/parent */
+#if CONFIG_BUILD_WITH_TFM
+	#define PM_ADDRESS_OFFSET (PM_MCUBOOT_PAD_SIZE + PM_TFM_SIZE)
+#else
+	#define PM_ADDRESS_OFFSET (PM_MCUBOOT_PAD_SIZE)
+#endif
+
+#ifdef CONFIG_MCUBOOT
+	/* lib is part of MCUboot -> operate on the primary application slot */
+	#define ACTIVE_SLOT_FLASH_AREA_ID	PM_MCUBOOT_PRIMARY_ID
+#else
+	/* lib is part of the App -> operate on active slot */
+#if (PM_ADDRESS - PM_ADDRESS_OFFSET) == PM_MCUBOOT_PRIMARY_ADDRESS
+	#define ACTIVE_SLOT_FLASH_AREA_ID	PM_MCUBOOT_PRIMARY_ID
+#elif (PM_ADDRESS - PM_ADDRESS_OFFSET) == PM_MCUBOOT_SECONDARY_ADDRESS
+	#define ACTIVE_SLOT_FLASH_AREA_ID	PM_MCUBOOT_SECONDARY_ID
+#else
+	#error Missing partition definitions.
+#endif
+#endif /* CONFIG_MCUBOOT */
+#endif /* CONFIG_MCUBOOT_APPLICATION_IMAGE_NUMBER != -1 */
+
+#else
+
 #if defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_RAM_LOAD)
 /* For RAM LOAD mode, the active image must be fetched from the bootloader */
 #define ACTIVE_SLOT_FLASH_AREA_ID boot_fetch_active_slot()
@@ -57,6 +99,8 @@ enum IMAGE_INDEXES {
 /* Get active partition. zephyr,code-partition chosen node must be defined */
 #define ACTIVE_SLOT_FLASH_AREA_ID DT_FIXED_PARTITION_ID(DT_CHOSEN(zephyr_code_partition))
 #endif
+
+#endif /* USE_PARTITION_MANAGER */
 
 /*
  * Raw (on-flash) representation of the v1 image header.